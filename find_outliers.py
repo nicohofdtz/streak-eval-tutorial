48241f028daf7ac58f40d2f30c4ac47c24370765
Nico Hofeditz <nicohofeditz@gmail.com>
2022-05-13 11:05:22 +0200

    Exclude nnn values from mean calculation
    Exclude next-nearest neighbours from the calculation
    of the mean and stdev.
    Fix not correcting outliers if two or more neighbouring pixels were off.

--- find_outliers.py
+++ find_outliers.py
@@ -2,26 +2,27 @@ import numpy as np
 import pandas as pd
 from typing import Tuple
 from copy import deepcopy as dc
-from streakimage2.streakimage import StreakImage
+from streakimage import StreakImage
 
-def get_mean(df: pd.DataFrame, poi: Tuple[int]) -> Tuple[float]:
+def get_mean(df: pd.DataFrame, ex_vals: list[float]) -> Tuple[float, float]:
     """
     Returns the mean and standard deviation of df without poi.
     """
     values = [val for ar in df.values for val in ar]
-    values.remove(
-        df.iloc[poi]
-    )  # remove value of point of interest, only once if contained multiple times
+    for ex in ex_vals:
+        values.remove(
+            ex
+        )  # remove value of point of interest, only once if contained multiple times
     val_array = np.array(values)
     return (val_array.mean(), val_array.std())
 
 
-def find_outliers(img: StreakImage, stdev_factor=4.0, window_size: int = 2, key=None):
+def find_outliers(img: StreakImage, stdev_factor=4.0, window_size: int = 2, key=None, exclude_nnn=True):
     """
     Finds ouliers and replace them with the mean value of their environment.
 
     Each cell in the dataframe is compared to its environment. The environment comprises the window_size rows and columns arround the cell.
-    The mean of the environment (without the value of the cell) is calculated and if the value of the cell deviated more than stdev_factor standard deviations from that mean, it is replaced by the mean.
+    The mean of the environment (without the value of the cell and - if exclude_nnn - next-nearest neighbours) is calculated and if the value of the cell deviates more than stdev_factor standard deviations from that mean, it is replaced by the mean.
 
     Parameters:
         df (pd.DataFrame): The DataFrame to digest.
@@ -31,19 +32,22 @@ def find_outliers(img: StreakImage, stdev_factor=4.0, window_size: int = 2, key=
     img_new = dc(img)
     df = img.data
     df_new = img_new.data
-    for idx in range(0, df.shape[0]):
-        for col in range(0, df.shape[1]):
+    xrange = range(0, df.shape[0])
+    yrange = range(0, df.shape[1])
+    for idx in xrange:
+        for col in yrange:
             a = idx - window_size if idx - window_size > 0 else 0
             b = idx + window_size + 1
             c = col - window_size if col - window_size > 0 else 0
             d = col + window_size + 1
             subset = df.iloc[a:b, c:d]
 
-            poi = (
-                idx if idx < window_size else window_size,
-                col if col < window_size else window_size,
-            )
-            env_mean, env_std = get_mean(subset, poi)
+            if exclude_nnn:
+                ex_vals = [df.iloc[pa,pb] for pa in [idx-1, idx, idx+1] for pb in [col-1, col, col+1] if pa in xrange and pb in yrange] #values of all nnn
+            else:
+                ex_vals = [df.iloc[idx, col]]
+
+            env_mean, env_std = get_mean(subset, ex_vals)
 
             if (
                 df.iloc[idx, col]
